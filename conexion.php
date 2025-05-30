bb62e21eb9956b39c8473006382b695c4be4c83c
ivanFRivas <151638610+ivanFRivas@users.noreply.github.com>
2025-05-30 14:32:20 -0700

    Update conexion.php
    nueva act

--- conexion.php
+++ conexion.php
@@ -1,15 +1,16 @@
 <?php
-$host = getenv("DB_HOST");        // Debes tener una variable de entorno llamada DB_HOST
-$dbname = getenv("DB_NAME");      // Y otra llamada DB_NAME
-$user = getenv("DB_USER");        // Variable de entorno para el usuario
-$password = getenv("DB_PASSWORD");// Variable de entorno para la contraseña
-$port = getenv("DB_PORT");        // Variable de entorno para el puerto
+$host = getenv("DB_HOST");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$port = getenv("DB_PORT");
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
-
